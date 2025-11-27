fd27dfd29a0251c3b108a3e1cbabd04df8af364f
sagar kamankar <sagarkamankar26@gmail.com>
2025-11-27 15:56:36 +0530

    changes in settings.py file

--- MySite/settings.py
+++ MySite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-@=#a4u!*jqm93(w=ic@0jp+9b1%wphfpcf_rzbj)f#!dg_@1a%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hackersaggyhacker.pythonanywhere.com',
+    'www.hackersaggyhacker.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -128,8 +131,9 @@ STATICFILES_DIRS = [
     BASE_DIR,"static"
 ]
 
-MEDIA_ROOT=BASE_DIR/"media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL="/media/"
+
 
 
